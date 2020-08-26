dfe8926cd0fc882b4acb77119dff4dbb50f00bfb
asincov <sashasintsov@mail.ru>
2020-08-26 21:31:59 +0300

    4 and 5 practices

--- a_sintsov_practices/task_4.py
+++ a_sintsov_practices/task_4.py
@@ -1,31 +1,51 @@
 # 1
 def fizzBuzz():
-    line = ""
-    fizz = "Fizz"
-    buzz = "Buzz"
-    for i in range(1, 100):
-        if (i % 3 == 0 and i % 5 == 0):
-            line = line.replace(line, fizz + buzz)
-        elif (i % 3 == 0):
-            line = line.replace(line, fizz)
-        elif (i % 5 == 0):
-            line = line.replace(line, buzz)
-        else:
-            line = line.replace(line, str(i))
-        print(line)
-
+    print("".join(str(el)+" " for el in ["FizzBuzz" if (x % 3 == 0 and x % 5 == 0)
+                                                    else ("Fizz" if (x % 3 == 0)
+                                                    else ("Buzz" if (x % 5 == 0)
+                                                    else x)) for x in range(1, 101)]))
 
 # 2
-def numberToDigits(number: int):
-    n = number
-    i = len(str(n)) - 1
-    count = 1
-    while (n != 0):
-        print("Цифра {count} равна {value}".format(count=count, value=(n // (10 ** i))))
-        n %= (10 ** i)
-        i -= 1
-        count += 1
+def numberToDigits(num: int):
+    print(*list(str(num)), sep = "\n")
+
+
+
+# 3
+def sort(arr):
+    for i in range(0, len(arr) - 1):
+        lst = arr[i::]
+        x = lst.index(min(lst))
+        lst[0], lst[x] = lst[x], lst[0] #почему не работает lst[0], lst[lst.index(min(lst))] = lst[lst.index(min(lst))], lst[0]
+        arr[i::] = lst
+    return arr
+
+# 4
+def tabToSpace(fileDirection: str, switcher: int):
+    d = {"    " : "\t"}
+    f = open(fileDirection, "r")
+    lst = []
+    for line in f:
+        for k, v in d.items():
+            if switcher is 1:
+                line = line.replace(k, v)
+            elif switcher is 2:
+                line = line.replace(v, k)
+            lst.append(line)
+    f = open(fileDirection, "w")
+    f.write("".join(lst))
+    f.close()
+
+#5
+def templates(line: str, templates: dict):
+    for k, v in templates.items():
+        line = line.replace(k, v)
+    return line
+
 
 
 fizzBuzz()
 numberToDigits(10819)
+print(sort([0, 3, 24, 2, 3, 7]))
+tabToSpace("./test.txt", 1)
+print(templates("qwe asd zxc", {"q":"Q", "w": "W", "e": "E"}))
